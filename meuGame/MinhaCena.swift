cd653e35433064c19c8a49a65f226293065f1785
Gabriel moreira <bielfire@gmail.com>
2019-11-25 21:18:46 -0300

    Adicionado física aos objetos;  Criado o evento de tocar na tela; Feito limites para Debug e Limites da Cena; Criando um Arquivo de Emissor de Partículas; Feito evento Update e DidSimulatePhysics; Criado e configurado o Emissor de Partículas Programaticamente; Xcode Swift e SpriteKit - Considerações Finais e Próximos Passos.

--- meuGame/MinhaCena.swift
+++ meuGame/MinhaCena.swift
@@ -10,106 +10,95 @@ import SpriteKit
 
 class MinhaCena: SKScene {
     
-    enum PlayerAnimationKey: String {
-        case walking
-        case energizing
-    }
-
-    var playerSpriteNode: SKSpriteNode?
-    var toUpTextures: [SKTexture] = []
-    var toDownTextures: [SKTexture] = []
+    let retangulo = SKShapeNode(rectOf: CGSize(width: 100, height: 100))
+    let bolinha = SKShapeNode(circleOfRadius: 25)
+    let particula = SKEmitterNode(fileNamed: "minhaParticula.sks")
     
     override func didMove(to view: SKView) {
-        self.backgroundColor = UIColor.black
+        self.backgroundColor = UIColor.white
         
-        let texture = SKTexture(imageNamed: "player")
-        let playerSpriteSheet = SpriteSheet(texture: texture, rows: 4, columns: 4, spacing: 0, margin: 0)
-
-        for column in 0...3 {
-            if let texture = playerSpriteSheet.textureForColumn(column: column, row: 2) {
-                toUpTextures.append(texture)
-            }
-            
-            if let texture = playerSpriteSheet.textureForColumn(column: column, row: 3) {
-                toDownTextures.append(texture)
-            }
-        }
-
-        playerSpriteNode = SKSpriteNode(texture: toDownTextures.first)
-        playerSpriteNode?.position.x = self.frame.midX
-        playerSpriteNode?.position.y = self.frame.midY
-
-        if let check = playerSpriteNode {
-            self.addChild(check)
+        retangulo.fillColor = .blue
+        retangulo.strokeColor = .green
+        retangulo.lineWidth = 3
+        retangulo.position = CGPoint(x: 200, y: 300)
+        self.addChild(retangulo)
+        
+        retangulo.physicsBody = SKPhysicsBody(rectangleOf: retangulo.frame.size)
+        retangulo.physicsBody?.isDynamic = true
+        retangulo.physicsBody?.allowsRotation = true
+        
+        let ret = CGRect(x: 0, y: 0, width: frame.width - 20, height: 10)
+        let chao = SKShapeNode(rect: ret)
+        chao.fillColor = .yellow
+        let centerChao = CGPoint(x: (frame.width - 20) / 2, y: 5)
+        chao.physicsBody = SKPhysicsBody(rectangleOf: chao.frame.size, center: centerChao)
+        chao.physicsBody?.isDynamic = false
+        chao.position.x = 10
+        chao.position.y = 20
+        addChild(chao)
+        
+        bolinha.fillColor = .red
+        bolinha.strokeColor = .green
+        bolinha.lineWidth = 3
+        bolinha.position = CGPoint(x: 100, y: 300)
+        self.addChild(bolinha)
+        
+        bolinha.physicsBody = SKPhysicsBody(circleOfRadius: bolinha.frame.size.width / 2)
+        bolinha.physicsBody?.isDynamic = true
+        bolinha.physicsBody?.allowsRotation = true
+        
+        let borderBody = SKPhysicsBody(edgeLoopFrom: self.frame)
+        borderBody.friction = 0
+        self.physicsBody = borderBody
+        
+        if let check = particula {
+            addChild(check)
         }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let location = touches.first?.location(in: self) {
-            let halfHeight = size.height * 0.5 // metade da altura
-            
-            if location.y > halfHeight {
-                // acima da metada da alture
-                
-                playerSpriteNode?.removeAction(forKey: PlayerAnimationKey.walking.rawValue)
-                
-                let animateTextureAction = SKAction.animate(with: toUpTextures, timePerFrame: 0.1)
-                let repeatAction = SKAction.repeatForever(animateTextureAction)
-                
-                playerSpriteNode?.run(repeatAction, withKey: PlayerAnimationKey.walking.rawValue)
-                
-            } else if location.y < halfHeight {
-                // abaixo da metada da alture
-                
-                playerSpriteNode?.removeAction(forKey: PlayerAnimationKey.walking.rawValue)
-                
-                let animateTextureAction = SKAction.animate(with: toDownTextures, timePerFrame: 0.1)
-                let repeatAction = SKAction.repeatForever(animateTextureAction)
-                
-                playerSpriteNode?.run(repeatAction, withKey: PlayerAnimationKey.walking.rawValue)
-            }
-        }
+        bolinha.physicsBody?.velocity = CGVector.zero
+        bolinha.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 100))
         
+        retangulo.physicsBody?.velocity = CGVector.zero
+        retangulo.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 500))
+        
+        for touch in (touches) {
+            let location = touch.location(in: self)
+            print("PosX: \(location.x) PosY: \(location.y)")
+            criaParticulasPenas(CGPoint(x: location.x, y: location.y))
+        }
+    }
+    
+    override func update(_ currenTime: TimeInterval) {
+    }
+    
+    override func didSimulatePhysics() {
+        particula?.position = bolinha.position
+    }
+    
+    func criaParticulasPenas(_ p:CGPoint) {
+        let peninha: SKTexture = SKTexture(imageNamed: "pena")
+        let minhaParticula: SKEmitterNode = SKEmitterNode()
+        minhaParticula.position = CGPoint(x: p.x, y: p.y)
+        minhaParticula.particleBirthRate = 100
+        minhaParticula.numParticlesToEmit = 7
+        minhaParticula.particleLifetime = 1.3
+        minhaParticula.particleTexture?.filteringMode = .nearest
+        minhaParticula.xAcceleration = 0
+        minhaParticula.yAcceleration = 0
+        minhaParticula.particleSpeed = 100
+        minhaParticula.particleSpeedRange = 200
+        minhaParticula.particleRotationSpeed = -10
+        minhaParticula.particleRotationRange = 4
+        minhaParticula.emissionAngle = CGFloat(Double.pi * 2)
+        minhaParticula.emissionAngleRange = CGFloat(Double.pi * 2)
+        minhaParticula.particleColorAlphaSpeed = 0.1
+        minhaParticula.particleRotationRange = 1
+        minhaParticula.particleAlphaSequence = SKKeyframeSequence(keyframeValues: [1,0], times: [0,1])
+        minhaParticula.particleScaleSequence = SKKeyframeSequence(keyframeValues: [3,0.5], times: [0,1])
+        self.addChild(minhaParticula)
+        minhaParticula.run(SKAction.sequence([SKAction.wait(forDuration: 2), SKAction.removeFromParent()]))
     }
 }
 
-
-
-
-
-
-
-
-        //        let bolinha:SKShapeNode = SKShapeNode(circleOfRadius:25)
-        //        bolinha.fillColor = .red
-        //        bolinha.strokeColor = .green
-        //        bolinha.lineWidth = 3
-        //        bolinha.position.x = 100
-        //        bolinha.position.y = 100
-        //        self.addChild(bolinha)
-        //
-        //        let retangulo:SKShapeNode = SKShapeNode(rectOf: CGSize(width: 100, height: 100))
-        //        retangulo.fillColor = .blue
-        //        bolinha.strokeColor = .green
-        //        retangulo.lineWidth = 3
-        //        retangulo.position.x = 200
-        //        retangulo.position.y = 100
-        //        self.addChild(retangulo)
-        
-//        let meuSprite: SKSpriteNode = SKSpriteNode(imageNamed: "megaman")
-//        meuSprite.position.x = self.frame.midX
-//        meuSprite.position.y = self.frame.midY
-//        self.addChild(meuSprite)
-//
-//        let point = CGPoint(x: 300, y: 100.0)
-//        let acaoMover: SKAction = SKAction.move(to: point, duration: 2.0)
-//        let acaoFade: SKAction = SKAction.fadeOut(withDuration: 1.0)
-//        let _: SKAction = SKAction.wait(forDuration: 2.0)
-//        let _: SKAction = SKAction.removeFromParent()
-//        let sequencia: SKAction = SKAction.sequence([acaoMover ,acaoFade])
-//        meuSprite.run(sequencia)
-//
-//        let acaoVai = SKAction.moveBy(x: 100, y: 200, duration: 1.0)
-//        let acaoVolta = SKAction.moveBy(x: -100, y: -200, duration: 1.0)
-//        meuSprite.run(SKAction.repeatForever(SKAction.sequence([acaoVai, acaoVolta])))
-        
